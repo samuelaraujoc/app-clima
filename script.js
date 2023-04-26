f803216fdb5a52a8d9b781c1122b2b65e84e269c
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2023-04-26 17:22:49 -0300

    Concluindo branch

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const keyy = "e6d1cd0be42b872c6499870b98ad9c28"
 
+/* Ao pressionar a tecla Enter também vai chamar a função que conecta a API */
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      document.querySelector('.button-search').click();
+    }
+  });
+
+/* Limpa o imput depois de chamar a funsão */
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      document.querySelector('.input-city').value ="";
+    }
+  });
+
+/* Capturando a cidade digitada pelo usuário e armazenando da variavel City, que vai ser enviada para a
+function searchCity que irá realizar uma busca na API e trazer o resultado do servidor, em seguida o convertando para Json */
+
 function captureCity() {
     const city = document.querySelector(".input-city").value
     searchCity(city)
 }
 
-/* Pegando a cidade digitada pelo usuário e armazenando da variavel City, que vai ser enviada para a
-function searchCity que irá realizar uma busca na API e trazer o resultado do servidor, em seguida o convertando para Json */
 async function searchCity(city) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyy}&lang=pt_br&units=metric`).then(response => response.json())
 
     dadosServer(dados)
 }
 
-/* Pegando os dados buscados no ervidor e aplicando no HTML (arvore DOM) */
+/* Pegando os dados buscados no Servidor e aplicando no HTML (arvore DOM) */
 function dadosServer(dados) {
     document.querySelector(".class-city").innerHTML = dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "º"
